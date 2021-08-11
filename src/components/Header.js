05dc93fc1d5f61ae7d5a676a9cb4a19be6310938
Your Name <33578391+modext@users.noreply.github.com>
2021-08-11 10:33:49 +0100

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Header = () => {
+    return (
+        <>
+            <h1>Hangman</h1>
+            <p>Find the hidden word -Enter a letter-</p>
+        </>
+    )
+}
+ export default Header;
